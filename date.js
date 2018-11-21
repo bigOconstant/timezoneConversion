b70504e8843b74b94337c2493fd9a726c70d4988
Caleb McCarthy <c.mccarthy@vivifyhealth.com>
2018-11-21 11:13:13 -0600

    Fix formula

--- date.js
+++ date.js
@@ -1,7 +1,7 @@
 var moment = require('moment-timezone');
 
 
-var PatientTimeZone = "America/Los_Angeles";
+var PatientTimeZone = "America/New_York";
 var format = "dddd, MMMM D YYYY, h:mm:ss a";
 
 var format1 = moment(new Date(),format).tz(PatientTimeZone);
@@ -18,7 +18,7 @@ if(them < us){
     console.log(moment(new Date).utcOffset(us - them +us).format('YYYY-MM-DD HH:mm')); 
 }
 else if(them > us){
-    console.log(moment(new Date).utcOffset(us + them -us).format('YYYY-MM-DD HH:mm'));
+    console.log(moment(new Date).utcOffset(them).format('YYYY-MM-DD HH:mm'));
 }
 else {// No changes nessesary
     console.log(moment(new Date).format('YYYY-MM-DD HH:mm'));
