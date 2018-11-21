12e2a017480343bc7c5d3d8d8cb9c801cda7e30b
Caleb McCarthy <c.mccarthy@vivifyhealth.com>
2018-11-21 11:16:18 -0600

    reduce

--- date.js
+++ date.js
@@ -1,7 +1,7 @@
 var moment = require('moment-timezone');
 
 
-var PatientTimeZone = "America/New_York";
+var PatientTimeZone = "America/Los_Angeles";
 var format = "dddd, MMMM D YYYY, h:mm:ss a";
 
 var format1 = moment(new Date(),format).tz(PatientTimeZone);
@@ -15,7 +15,7 @@ console.log("them:"+them);
 
 
 if(them < us){
-    console.log(moment(new Date).utcOffset(us - them +us).format('YYYY-MM-DD HH:mm')); 
+    console.log(moment(new Date).utcOffset(2 * us - them).format('YYYY-MM-DD HH:mm')); 
 }
 else if(them > us){
     console.log(moment(new Date).utcOffset(them).format('YYYY-MM-DD HH:mm'));
